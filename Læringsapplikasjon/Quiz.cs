67347409df88af421116b48762c7c8c1ebc06c42
torjeikenes <torje.n.eikenes@gmail.com>
2018-02-07 22:58:48 +0100

    Gigantisk commit

---
+++ Læringsapplikasjon/Quiz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Læringsapplikasjon
+{
+    class Quiz
+    {
+        private string file;
+        private string question;
+
+        public Quiz(string jsonFile,int qstNr)
+        {
+            file = jsonFile;
+
+        }
+    }
+}
